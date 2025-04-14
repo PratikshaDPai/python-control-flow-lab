ce39e93766079e98a4959269253f4997812688ef
ppai <1312pratiksha@gmail.com>
2025-04-14 16:40:29 -0700

    Add Exercise 3 Solution

--- exercises.py
+++ exercises.py
@@ -42,15 +42,16 @@ def check_letter():
     lowercase_letter=letter.lower()
     vowels=['a','e','i','o','u']
     consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+    done=False
     for vowel in vowels:
         if lowercase_letter==vowel:
             print(f"The letter {lowercase_letter} is a vowel")
-            break
+            done=True
     for consonant in consonants:
         if lowercase_letter==consonant:
             print(f"The letter {lowercase_letter} is a consonant")
-            break
-    else:
+            done=True
+    if done==False:
         print("Invalid letter")
 
 
@@ -91,3 +92,42 @@ def check_voting_eligibility():
 
 # Call the function
 check_voting_eligibility()
+
+# Exercise 3: Calculate Dog Years
+#
+# Write a Python function named `calculate_dog_years` that calculates a dog's age in dog years.
+# Fill in the logic to perform the calculation inside the function.
+#
+# Function Details:
+# - Prompt the user to enter a dog's age: "Input a dog's age: "
+# - Calculate the dog's age in dog years:
+#      - The first two years of the dog's life count as 10 dog years each.
+#      - Each subsequent year counts as 7 dog years.
+# - Print the calculated age: "The dog's age in dog years is xx."
+# - Replace 'xx' with the calculated dog years.
+#
+# Hints:
+# - Use the `input()` function to capture user input.
+# - Convert the string input to an integer using `int()`.
+# - Apply conditional logic to perform the correct age calculation based on the dog's age.
+
+def calculate_dog_years():
+    # Your control flow logic goes here
+  try:
+    age=int(input("Enter your dog's age in years: "))
+    if(age<0):
+       print("Please enter a positive age")
+       age=int(input("Enter your dog's age in years: "))
+    if(age<=2):
+        dog_age=age*10
+    else:
+        dog_age=(age-2)*7+20
+    print(f"The dog's age in dog years is {dog_age}")
+    
+  except ValueError as e:
+    # Code to handle the ValueError
+    print("ValueError:", e)
+
+# Call the function
+calculate_dog_years()
+
